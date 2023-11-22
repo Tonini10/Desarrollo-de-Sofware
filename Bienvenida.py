de31aaf9e87d986d363b58c958cb3b63a51183ff
Tonini10 <150728462+Tonini10@users.noreply.github.com>
2023-11-22 11:58:56 -0500

    Add files via upload

---
+++ Bienvenida.py
@@ -0,0 +1,7 @@
+# Bienvenida y respuesta a un saludo en Python
+
+# Solicitar al usuario que ingrese su nombre
+nombre = input("¡Bienvenido! Por favor, ingresa tu nombre: ")
+
+# Mostrar un mensaje de bienvenida personalizado
+print(f"\nHola {nombre}! Bienvenido al juego. ¡Diviértete!")
