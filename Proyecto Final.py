5a5a80f380d44618d3da13565e6d0e11601f649f
Tonini10 <150728462+Tonini10@users.noreply.github.com>
2023-12-16 15:39:41 -0500

    Update Proyecto Final.py

--- Proyecto Final.py
+++ Proyecto Final.py
@@ -1,10 +1,6 @@
-# Bienvenida y respuesta a un saludo en Python
-
-# Solicitar al usuario que ingrese su nombre
-nombre = input("¡Bienvenido! Por favor, ingresa tu nombre: ")
 
 # Mostrar un mensaje de bienvenida personalizado
-print(f"\nHola {nombre}! Bienvenido al juego. ¡Diviértete!")
+print(! Bienvenido al juego. ¡Diviértete!")
 
 # Opciones para el jugador
 opcion_piedra = "piedra"
